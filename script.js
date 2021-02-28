24f49c7e02bc7a308db5a2934c1d2f9a0d9375a1
Your Name <you@example.com>
2021-03-01 01:23:09 +0300

    add validation rule for the contact section

--- script.js
+++ script.js
@@ -13,3 +13,29 @@ $(document).ready(function() {
     });
 });
 
+var btn = document.getElementById("mySubw");
+
+// When the user clicks on the button, open the modal
+btn.onclick = function(event) {
+    // Validate form for entry
+    var x = document.forms["myForm"]["contact-name"].value;
+    if (x == "") {
+        alert("Name not entered");
+        return false;
+    }
+    var y = document.forms["myForm"]["contact-email"].value;
+    if (y == "") {
+        alert("Email not entered");
+        return false;
+    }
+    var z = document.forms["myForm"]["contact-message"].value;
+    if (z == "") {
+        alert("You have no message to send");
+        return false;
+    }
+    var person1Input = $("input#contact-name").val();
+    $("span#outputName").append(person1Input);
+
+    $('.toast').toast('show');
+    event.preventDefault();
+}
