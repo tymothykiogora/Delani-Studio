7bc5068d050b1815288b751e68c67cd9e466d276
Your Name <you@example.com>
2021-03-01 01:21:12 +0300

    add jquery file

---
+++ script.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+    $(".onCrack1").click(function() {
+        $("#click1").toggle();
+        $("#magic1").toggle();
+    });
+    $(".onCrack2").click(function() {
+        $("#magic2").toggle();
+        $("#click2").toggle();
+    });
+    $(".onCrack3").click(function() {
+        $("#magic3").toggle();
+        $("#click3").toggle();
+    });
+});
+
